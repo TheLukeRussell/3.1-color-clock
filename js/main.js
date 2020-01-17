e406c3114644f8255725209f3c18be562fee3b27
Luke Russell <thelukerussel21@gmail.com>
2020-01-17 09:01:10 -0500

    deleted uneccessary code

--- js/main.js
+++ js/main.js
@@ -86,17 +86,7 @@
     //*********************************************
     // document.addEventListener(mouseover, function startTime(){document.quertSelector('#clock').textContent = '#' + hr + min + sec;});
     // `${hr}:${min}:${sec}`;
-// let underline = document.querySelector('#underline')
-//
-//     let percentage  = ((sec) * 5);
-//
-//     let width = (percentage);
-//
-//
-//
-//     // let barWidth = (percentage * 100).toString();
-//     underline.style.width = (`${width}px`);
-//
+
   let timeBar = (sec/60);
   let width = timeBar * 500;
   let pixelWidth = width + "px"
@@ -113,59 +103,10 @@
   startTime();
   setInterval(startTime, 1000);
 
-  // function changeColor()
-  // {
-  //     let date = new Date();
-  //     let hr = date.getHours();
-  //     let min = date.getMinutes();
-  //     let sec = date.getSeconds();
-  //
-  //     hr = checkTime(hr);
-  //     min = checkTime(min);
-  //     sec = checkTime(sec);
-
   //*****watching to see if color still changes**
   // document.querySelector('.color').textContent = '#' + hr + min + sec;
   //*********************************************
-  // let clock = document.querySelector('#clock')
 
-  // clock.addEventListener("mouseout" function()){
-  //   clock.clockContent = '.color'
-  // })
-  // clock.addEventListener("mouseover" function()){
-  //   clock.clockContent = '.color'
-
-  //     document.querySelector('.container').style.backgroundColor = '#' + hr + min + sec;
-  //
-  //
-  //
-  //     function checkTime(t) {
-  //         if (t < 10) {
-  //             t = "0" + t;
-  //         }
-  //         return t;
-  //     }
-  //
-  // }
-  //
-  // changeColor();
-  // setInterval(changeColor, 1000);
-
-  // function timeBar() {
-  //   let date = new Date();
-  //
-  //   let sec = date.getSeconds();
-  //   sec = checkTime(sec);
-  //
-  //   let timeBar = (sec/60);
-  // //
-  // document.querySelector('#underline').style.width = sec;
-  // //
-  // //     }
-  // //
-  // //
-  // timeBar();
-  // setInterval(timeBar, 1000);
 
   // identified html element to target clock //
   // let $time
